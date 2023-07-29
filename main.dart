76e75413305e811bc77b4d5e6997ab88486ed9bb
wishma khurram <wishmakhurram84@gmail.com>
2023-07-29 11:02:40 +0500

    dart lang

---
+++ main.dart
@@ -0,0 +1,58 @@
+
+import 'dart:io';
+main(){
+
+
+
+
+
+}
+
+// const constVal=0;
+// print(constVal);
+// print(constVal.runtimeType);
+
+
+// String one=1.738393.toStringAsFixed(3);
+// assert(one==1.738);
+
+
+
+// var one=int.parse('1');
+// assert(one==1);
+
+
+// var multi=''' this
+// is
+// multiline
+// variable
+// working  ''';
+// print(multi);
+
+
+
+// int age=35;
+// String myAge='my age is $age';
+// print(myAge);
+
+
+//   var val=r'it is \n what it is';
+//   print(val);
+
+
+// dynamic weak=9;
+// print(weak);
+// weak="every";
+// print(weak);
+
+
+
+// stdout.writeln('enter your name? ');
+// String name = stdin.readLineSync()!;
+// print('my name is $name');
+
+   
+
+//    var firstName="Wishma";
+//    var lastName="khurram";
+//    print(firstName+""+lastName);
